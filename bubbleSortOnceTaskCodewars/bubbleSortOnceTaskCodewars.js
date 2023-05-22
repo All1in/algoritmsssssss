2972a7a941dae108ec3ac88fc65d8618123069fc
All1in <bm3023068@gmail.com>
2023-05-22 18:44:28 +0300

    task from codewars for algs 7 kyu

---
+++ bubbleSortOnceTaskCodewars/bubbleSortOnceTaskCodewars.js
@@ -0,0 +1,11 @@
+const bubbleSortOnce = arr => {
+    let newArr = arr.slice()
+    for (let i = 0; i < newArr.length - 1; i++) {
+        if (newArr[i] > newArr[i + 1]) {
+            [newArr[i], newArr[i + 1]] = [newArr[i + 1], newArr[i]];
+        }
+    }
+    return newArr
+}
+
+console.log(bubbleSortOnce([9, 7, 5, 3, 1, 2, 4, 6, 8]))
